3d805179e9a395a1d727add816a44dbf6281d67d
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-04 17:53:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,23 +28,19 @@ class Owner
   end
   
   def cats
-    Cat.all.select{|cat| cat.owner == self}
+    @cats = Cat.all.select{|cat| cat.owner == self}
   end
   
   def dogs
-    Dog.all.select{|dog| dog.owner == self}
+    @dogs = Dog.all.select{|dog| dog.owner == self}
   end
   
   def buy_cat(pet_name)
     new_cat = Cat.new(pet_name, owner = self)
-    #new_cat.owner = self
-    @cats << new_cat
   end
   
   def buy_dog(pet_name)
     new_dog = Dog.new(pet_name, owner = self)
-    #new_dog.owner = self
-    @dogs << new_dog
   end
   
   def walk_dogs
