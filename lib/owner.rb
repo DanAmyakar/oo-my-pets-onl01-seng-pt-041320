0e8bac098b5bb91a338e591b8ab6fc2526da60e3
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-04 17:21:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -38,14 +38,12 @@ class Owner
   end
   
   def buy_cat(pet_name)
-    binding.pry
     new_cat = Cat.new(pet_name, owner = self)
     #new_cat.owner = self
     @cats << new_cat
   end
   
   def buy_dog(pet_name)
-    binding.pry
     new_dog = Dog.new(pet_name, owner = self)
     #new_dog.owner = self
     @dogs << new_dog
